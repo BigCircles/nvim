456ffbad01a53d1bcdc389269275f00f622fe53a
BigCircles <Yatchd@gmail.com>
2022-10-24 20:49:32 -0400

    updates

--- lua/strem/cmp.lua
+++ lua/strem/cmp.lua
@@ -60,9 +60,15 @@ local cmp = require'cmp'
   })
 
   -- Setup lspconfig.
-  local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+  local capabilities = require('cmp_nvim_lsp').default_capabilities()
   -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
   require('lspconfig')['clangd'].setup {
+    capabilities = capabilities
+  }
+  require('lspconfig')['marksman'].setup {
+    capabilities = capabilities
+  }
+  require('lspconfig')['bashls'].setup {
     capabilities = capabilities
   }
 
--- lua/strem/lspconfig.lua
+++ lua/strem/lspconfig.lua
@@ -1,8 +1,31 @@
 -- for help do: :h vim.lsp.buf.*
 -- nvim .7+ lua keymapping setting
 
+require("nvim-lsp-installer").setup()
+
 require'lspconfig'.clangd.setup{
-	on_attach = function() 
+	on_attach = function()
+--	vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer=0})
+--	vim.keymap.set("i", "N", vim.lsp.buf.completion, {buffer=0})
+	end,
+}
+
+require'lspconfig'.marksman.setup{
+	on_attach = function()
+--	vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer=0})
+--	vim.keymap.set("i", "N", vim.lsp.buf.completion, {buffer=0})
+	end,
+}
+
+require'lspconfig'.bashls.setup{
+	on_attach = function()
+--	vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer=0})
+--	vim.keymap.set("i", "N", vim.lsp.buf.completion, {buffer=0})
+	end,
+}
+
+require'lspconfig'.sumneko_lua.setup{
+	on_attach = function()
 --	vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer=0})
 --	vim.keymap.set("i", "N", vim.lsp.buf.completion, {buffer=0})
 	end,
